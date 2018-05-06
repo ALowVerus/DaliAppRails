d6e0bc1828b0c84dcae331a3b7bc48918f7c03dd
ALowVerus <aidan.low98@gmail.com>
2018-05-06 23:09:38 +0000

    added searchability

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,5 +3,11 @@ class StaticPagesController < ApplicationController
   end
 
   def dalimembers
+    require 'open-uri'
+    parsed_json = JSON.parse(open("http://mappy.dali.dartmouth.edu/members.json").read)
+    @selected = parsed_json
+    if params[:term] != nil
+      @selected = @selected.select {|member| member['terms_on'].include? params[:term]}
+    end
   end
 end
